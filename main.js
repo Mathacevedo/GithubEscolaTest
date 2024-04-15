203bcbdd7b3bc007868d57eb9e1db1cb9af141f2
Mathacevedo <130381642+Mathacevedo@users.noreply.github.com>
2024-04-15 09:17:32 -0300

    w

--- main.js
+++ main.js
@@ -34,7 +34,11 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
  
+ if (tempoFinal>0){   
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}else{
+    return "prazo finalizado";
+}
 }
 
 function atualizaCronometro(){
